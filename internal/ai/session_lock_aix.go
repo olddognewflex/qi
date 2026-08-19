39d2075495c29326ce84dff06815efa377a7dd95
Raymond Doran <raymond@olddognewflex.com>
2026-08-19 02:48:05 -0500

    fix(ai): support planner leases on AIX
    Use AIX fcntl record locks instead of the unavailable flock API while preserving the existing lease semantics.\n\nRefs #63

--- internal/ai/session_lock_aix.go
+++ internal/ai/session_lock_aix.go
@@ -1,17 +1,23 @@
-//go:build darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
+//go:build aix
 
 package ai
 
 import (
 	"errors"
+	"io"
 	"os"
 
 	"golang.org/x/sys/unix"
 )
 
 func tryLockSession(file *os.File) error {
-	err := unix.Flock(int(file.Fd()), unix.LOCK_EX|unix.LOCK_NB)
-	if errors.Is(err, unix.EWOULDBLOCK) || errors.Is(err, unix.EAGAIN) {
+	lock := unix.Flock_t{
+		Type:   unix.F_WRLCK,
+		Whence: io.SeekStart,
+		Len:    1,
+	}
+	err := unix.FcntlFlock(file.Fd(), unix.F_SETLK, &lock)
+	if errors.Is(err, unix.EACCES) || errors.Is(err, unix.EAGAIN) {
 		return ErrSessionLeaseHeld
 	}
 	return err
